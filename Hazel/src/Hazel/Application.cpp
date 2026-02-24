3cddd5809127d1fc601e393803f82305803fef13
dumr3n <k3nteck@gmail.com>
2026-02-24 17:31:25 +0200

    add event system

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,5 +1,8 @@
 #include "Application.h"
 
+#include "Events/ApplicationEvent.h"
+#include "Log.h"
+
 namespace Hazel {
 
 	Application::Application()
@@ -13,6 +16,17 @@ namespace Hazel {
 	}
 
 	void Application::Run() {
+
+		WindowResizeEvent e(1280, 720);
+
+		if (e.IsInCategory(EventCategoryApplication))
+		{
+			HZ_TRACE(e.ToString());
+		}
+		if (e.IsInCategory(EventCategoryInput))
+		{
+			HZ_TRACE(e.ToString());
+		}
 		while (true);
 	}
 
