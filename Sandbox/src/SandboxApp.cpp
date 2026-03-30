50163fbf168c807bbfeb38326f263f997205bba0
Dumr3n <k3nteck@gmail.com>
2026-03-30 17:05:48 +0300

    Blending

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -175,6 +175,7 @@ public:
 		m_TextureShader.reset(Hazel::Shader::Create(textureShaderVertexSrc, textureShaderFragmentSrc));
 
 		m_Texture = Hazel::Texture2D::Create(std::string(ASSET_PATH) + "/textures/Checkerboard.png");
+		m_ChernoLogoTexture = Hazel::Texture2D::Create(std::string(ASSET_PATH) + "/textures/ChernoLogo.png");
 
 		std::dynamic_pointer_cast<Hazel::OpenGLShader>(m_TextureShader)->Bind();
 		std::dynamic_pointer_cast<Hazel::OpenGLShader>(m_TextureShader)->UploadUniformInt("u_Texture", 0);
@@ -220,6 +221,9 @@ public:
 
 		m_Texture->Bind();
 		Hazel::Renderer::Submit(m_TextureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
+		
+		m_ChernoLogoTexture->Bind(); 
+		Hazel::Renderer::Submit(m_TextureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
 
 		// Triangle
 		//Hazel::Renderer::Submit(m_Shader, m_VertexArray);
@@ -252,6 +256,7 @@ private:
 	Hazel::Ref<Hazel::VertexArray> m_SquareVA;
 	
 	Hazel::Ref<Hazel::Texture2D> m_Texture;
+	Hazel::Ref<Hazel::Texture2D> m_ChernoLogoTexture;
 
 
 	Hazel::OrthographicCamera m_Camera;
