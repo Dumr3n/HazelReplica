3e82ba2479b117692afeab21d93903ea9eab032c
Dumr3n <k3nteck@gmail.com>
2026-04-07 12:57:56 +0300

    Read shaders from file

--- Hazel/src/Hazel/Renderer/Shader.cpp
+++ Hazel/src/Hazel/Renderer/Shader.cpp
@@ -4,6 +4,16 @@
 #include "Renderer.h"
 #include "Platform/OpenGL/OpenGLShader.h"
 namespace Hazel {
+	Shader* Shader::Create(const std::string& filepath)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None:	HZ_CORE_ASSERT(false, "RendererAPI::None is not supported"); return nullptr;
+		case RendererAPI::API::OpenGL:	return new OpenGLShader(filepath);
+		}
+		HZ_CORE_ASSERT(false, "Unnknown RendererAPI");
+		return nullptr;
+	}
 
 	Shader* Shader::Create(const std::string& vertexSrc, std::string& fragmentSrc)
 	{
@@ -15,4 +25,5 @@ namespace Hazel {
 		HZ_CORE_ASSERT(false, "Unnknown RendererAPI");
 		return nullptr;
 	}
+
 }
