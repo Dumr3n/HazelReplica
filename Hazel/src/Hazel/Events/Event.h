38805e6bf265f376e0990aa64ecf5a3b2dbe833a
Dumr3n <k3nteck@gmail.com>
2026-04-07 19:03:58 +0300

    Add OrthographicCamera + fix Handled flag

--- Hazel/src/Hazel/Events/Event.h
+++ Hazel/src/Hazel/Events/Event.h
@@ -37,7 +37,7 @@ namespace Hazel {
 	class HAZEL_API Event
 	{
 	public:
-		bool Handled = true;
+		bool Handled = false;
 
 		virtual EventType GetEventType() const = 0;
 		virtual const char* GetName() const = 0;
